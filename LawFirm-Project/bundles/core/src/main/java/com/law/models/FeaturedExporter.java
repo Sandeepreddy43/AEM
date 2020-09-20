e1d7a130adb1f836c6780aa7245de3a93c9a4127
SandeepReddy43 <nccpnm2018@gmail.com>
2020-09-20 14:43:18 +0530

    exporter changes

--- LawFirm-Project/bundles/core/src/main/java/com/law/models/FeaturedExporter.java
+++ LawFirm-Project/bundles/core/src/main/java/com/law/models/FeaturedExporter.java
@@ -1,81 +1,61 @@
 package com.law.models;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
-import org.json.JSONArray;
 
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
 
-@Model(adaptables = Resource.class, resourceType = "lawfirm-project/components/content/feature-multi", adapters = ComponentExporter.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+import lombok.Getter;
+
+@Model(adaptables = Resource.class, resourceType = FeaturedExporter.RESOURCE_TYPE , adapters = ComponentExporter.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class FeaturedExporter implements ComponentExporter {
 
-	@Inject
-	private Resource navItems;
-
-	private List<ExporterBean> listData;
-
-	public List<ExporterBean> getListData() {
-		return listData;
+	static final String RESOURCE_TYPE  = "lawfirm-project/components/content/feature-multi";
+	
+	@Inject @Getter
+	List<ExporterBean>  navItems;
+	
+	@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+	interface ExporterBean{
+		
+		@Inject
+		String getTitle();
+		
+		@Inject
+		String getText1();
+		
+		@Inject
+		String getText2();
+		
+		@Inject
+		String getLink();
+		
+		@Inject
+		String getImglink();
+		
+		@Inject
+		Date getDate();
 	}
 
-	@PostConstruct
-	protected void init() {
-		System.out.println("First Line");
-		System.out.println("init------");
-		if (navItems != null) {
-			listData = new ArrayList<ExporterBean>();
-			System.out.println("featured - init() called");
-			Iterator<Resource> listChildren = navItems.listChildren();
-			while (listChildren.hasNext()) {
-				Resource res = listChildren.next();
-				ValueMap vMap = res.getValueMap();
-				System.out.println("\nValue Map");
-				System.out.println(vMap.get("title", String.class));
-				System.out.println(vMap.get("text1", String.class));
-				System.out.println(vMap.get("text2", String.class));
-				System.out.println(vMap.get("link", String.class));
-				System.out.println(vMap.get("imglink", String.class));
-				System.out.println(vMap.get("date", Date.class));
-				ExporterBean bean = new ExporterBean();
-				bean.setTitle(vMap.get("title", String.class));
-				bean.setCardtext1(vMap.get("text1", String.class));
-				bean.setCardtext2(vMap.get("text2", String.class));
-				bean.setLink(vMap.get("link", String.class));
-				bean.setImage(vMap.get("imglink", String.class));
-				bean.setDate(vMap.get("date", Date.class));
-				System.out.println("\nBean Getters");
-				System.out.println(bean.getCardtext1());
-				System.out.println(bean.getCardtext2());
-				System.out.println(bean.getD());
-				System.out.println(bean.getImage());
-				System.out.println(bean.getLink());
-				System.out.println(bean.getTitle());
-				System.out.println(bean.getDate());
-				listData.add(bean);
-			}
-		}
+	@Override
+	public String getExportedType() {
+		// TODO Auto-generated method stub
+		return RESOURCE_TYPE;
 	}
+	
 	
 	/* List - JSON Array - String*/
 //	JSONArray jsArray = new JSONArray(listData);
 //	String json = jsArray.toString();
-
-	@Override
-	public String getExportedType() {
-
-		return new JSONArray(listData).toString();
-	}
+	  
+	
 }
